00d582fc6d1d60f75f3860fdead8c03e366d5f26
gmanipon <gmanipon@jpl.nasa.gov>
2019-02-14 00:13:02 +0000

    remove imports not yet installed via install_requires

--- sdscli/__init__.py
+++ sdscli/__init__.py
@@ -2,8 +2,6 @@ from __future__ import unicode_literals
 from __future__ import print_function
 from __future__ import division
 from __future__ import absolute_import
-from future import standard_library
-standard_library.install_aliases()
 __version__ = "0.0.11"
 __url__ = "https://github.com/sdskit/sdscli"
 __description__ = "Command line interface for SDSKit"
