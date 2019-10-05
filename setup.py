8c73d1420ced8c93e6f68eba67348a0efc17bb28
gmanipon <gmanipon@jpl.nasa.gov>
2019-10-05 19:06:10 +0000

    revert: prompt_toolkit release 1.0.17 included files from 2.0 branch
    https://github.com/prompt-toolkit/python-prompt-toolkit/commit/c1c59daf404b8c60efdc29aad671f753dc4b1b0d

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
-    install_requires=['PyYAML>=5.1', 'Pygments>=2.4.0', 'prompt-toolkit==1.0.16',
+    install_requires=['PyYAML>=5.1', 'Pygments>=2.4.0', 'prompt-toolkit>=1.0,<2.0',
                       'tqdm>=4.32.1', 'backoff>=1.8.0', 'future>=0.17.1', 'requests>=2.22.0',
                       'kombu>=4.5.0', 'redis>=3.2.1', 'elasticsearch>=1.0.0,<2.0.0', 'boto3>=1.9.154',
                       'fabric3>=1.14.post1', 'Jinja2>=2.10.1'],
