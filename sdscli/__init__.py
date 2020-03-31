a3f435c368f3041abc1eb61a43054955ce367b29
DustinKLo <dustinlo.pcc@gmail.com>
2020-03-30 17:27:24 -0700

    bump version

--- sdscli/__init__.py
+++ sdscli/__init__.py
@@ -3,6 +3,6 @@ from __future__ import print_function
 from __future__ import division
 from __future__ import absolute_import
 
-__version__ = "0.2.9"
+__version__ = "0.2.10"
 __url__ = "https://github.com/sdskit/sdscli"
 __description__ = "Command line interface for SDSKit"
