465ac762c5f270d0e4d3e8cf3a74891950be6d95
Gerald Manipon <pymonger@gmail.com>
2020-04-16 19:13:01 -0700

    restore configuration tool (#53)

--- sdscli/adapters/__init__.py
+++ sdscli/adapters/__init__.py
@@ -8,10 +8,13 @@ from sdscli.log_utils import logger
 from hysds_commons.elasticsearch_utils import ElasticsearchUtility
 
 
-conf = SettingsConf()
-
-_mozart_es_url = "http://{}:9200".format(conf.get('MOZART_ES_PVT_IP'))
-_grq_es_url = "http://{}:9200".format(conf.get('GRQ_ES_PVT_IP'))
-
-mozart_es = ElasticsearchUtility(_mozart_es_url, logger)
-grq_es = ElasticsearchUtility(_grq_es_url, logger)
+try:
+    conf = SettingsConf()
+    
+    _mozart_es_url = "http://{}:9200".format(conf.get('MOZART_ES_PVT_IP'))
+    _grq_es_url = "http://{}:9200".format(conf.get('GRQ_ES_PVT_IP'))
+    
+    mozart_es = ElasticsearchUtility(_mozart_es_url, logger)
+    grq_es = ElasticsearchUtility(_grq_es_url, logger)
+except:
+    pass
