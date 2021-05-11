a362d93483620ea414fbce1d049939b53110a16e
Gerald Manipon <gmanipon@jpl.nasa.gov>
2021-05-11 10:47:40 -0700

    update to celery 5.0.5 (#81)
    * update celery CLI invocation for v5
    
    * bump version

--- sdscli/__init__.py
+++ sdscli/__init__.py
@@ -3,6 +3,6 @@ from __future__ import print_function
 from __future__ import division
 from __future__ import absolute_import
 
-__version__ = "1.0.11"
+__version__ = "1.1.0"
 __url__ = "https://github.com/sdskit/sdscli"
 __description__ = "Command line interface for SDSKit"
