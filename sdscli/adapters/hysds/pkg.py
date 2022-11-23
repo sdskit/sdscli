2ad3a645bc05c225ae5c1efcca17b701883bc31f
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-23 14:47:39 +0000

    Adding tarfile member sanitization to extractall()

--- sdscli/adapters/hysds/pkg.py
+++ sdscli/adapters/hysds/pkg.py
@@ -197,7 +197,26 @@ def import_pkg(args):
     outdir = os.path.dirname(tar_file)
     with tarfile.open(tar_file) as tar:
         export_name = tar.getnames()[0]
-        tar.extractall(outdir)
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(tar, outdir)
     export_dir = os.path.join(outdir, export_name)
     logger.debug("export_dir: %s" % export_dir)
 
