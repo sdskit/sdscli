e7e3d2edbabbad62b90af6c4efdb10e4c19cad97
dustinlo <dustin.k.lo@jpl.nasa.gov>
2023-09-11 17:26:08 -0700

    flipped logic around to make elasticseaech the default

--- sdscli/adapters/hysds/update.py
+++ sdscli/adapters/hysds/update.py
@@ -926,24 +926,7 @@ def import_kibana(comp='metrics'):
         bar.update()
         set_bar_desc(bar, 'copying over dashboards and scripts')
 
-        if fab.metrics_es_engine != "opensearch":
-            execute(fab.send_template_user_override, 'import_dashboard.sh.tmpl',
-                    '~/metrics/ops/kibana_metrics/import_dashboard.sh',
-                    '~/mozart/ops/sdscli/sdscli/adapters/hysds/files/kibana_dashboard_import',
-                    roles=[comp])
-            execute(fab.chmod, 755,
-                    '~/metrics/ops/kibana_metrics/import_dashboard.sh', roles=[comp])
-            execute(fab.copy, '~/.sds/files/kibana_dashboard_import/job-dashboards.json',
-                    '~/metrics/ops/kibana_metrics/job-dashboards.json', roles=[comp])
-            execute(fab.copy, '~/.sds/files/kibana_dashboard_import/worker-dashboards.json',
-                    '~/metrics/ops/kibana_metrics/worker-dashboards.json', roles=[comp])
-            execute(fab.copy, '~/.sds/files/kibana_dashboard_import/sdswatch-dashboards.json',
-                    '~/metrics/ops/kibana_metrics/sdswatch-dashboards.json', roles=[comp])
-            execute(fab.copy, '~/.sds/files/kibana_dashboard_import/wait-for-it.sh',
-                    '~/metrics/ops/kibana_metrics/wait-for-it.sh', roles=[comp])
-            execute(fab.chmod, 755,
-                    '~/metrics/ops/kibana_metrics/wait-for-it.sh', roles=[comp])
-        else:
+        if fab.metrics_es_engine == "opensearch":
             execute(fab.send_template_user_override, 'import_dashboard.sh.tmpl',
                     '~/metrics/ops/kibana_metrics/import_dashboard.sh',
                     '~/mozart/ops/sdscli/sdscli/adapters/hysds/files/opensearch_dashboards_import',
@@ -957,6 +940,23 @@ def import_kibana(comp='metrics'):
             execute(fab.copy, '~/.sds/files/opensearch_dashboards_import/sdswatch-dashboards.json',
                     '~/metrics/ops/kibana_metrics/sdswatch-dashboards.json', roles=[comp])
             execute(fab.copy, '~/.sds/files/opensearch_dashboards_import/wait-for-it.sh',
+                    '~/metrics/ops/kibana_metrics/wait-for-it.sh', roles=[comp])
+            execute(fab.chmod, 755,
+                    '~/metrics/ops/kibana_metrics/wait-for-it.sh', roles=[comp])
+        else:
+            execute(fab.send_template_user_override, 'import_dashboard.sh.tmpl',
+                    '~/metrics/ops/kibana_metrics/import_dashboard.sh',
+                    '~/mozart/ops/sdscli/sdscli/adapters/hysds/files/kibana_dashboard_import',
+                    roles=[comp])
+            execute(fab.chmod, 755,
+                    '~/metrics/ops/kibana_metrics/import_dashboard.sh', roles=[comp])
+            execute(fab.copy, '~/.sds/files/kibana_dashboard_import/job-dashboards.json',
+                    '~/metrics/ops/kibana_metrics/job-dashboards.json', roles=[comp])
+            execute(fab.copy, '~/.sds/files/kibana_dashboard_import/worker-dashboards.json',
+                    '~/metrics/ops/kibana_metrics/worker-dashboards.json', roles=[comp])
+            execute(fab.copy, '~/.sds/files/kibana_dashboard_import/sdswatch-dashboards.json',
+                    '~/metrics/ops/kibana_metrics/sdswatch-dashboards.json', roles=[comp])
+            execute(fab.copy, '~/.sds/files/kibana_dashboard_import/wait-for-it.sh',
                     '~/metrics/ops/kibana_metrics/wait-for-it.sh', roles=[comp])
             execute(fab.chmod, 755,
                     '~/metrics/ops/kibana_metrics/wait-for-it.sh', roles=[comp])
