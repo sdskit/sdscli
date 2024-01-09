59874cf04efa6ac016629925c12a0196a612d2a2
Mike Cayanan <michael.d.cayanan@jpl.nasa.gov>
2024-01-09 09:38:15 -0800

    bump version

--- sdscli/__init__.py
+++ sdscli/__init__.py
@@ -3,6 +3,6 @@ from __future__ import print_function
 from __future__ import division
 from __future__ import absolute_import
 
-__version__ = "1.3.0"
+__version__ = "1.3.1"
 __url__ = "https://github.com/sdskit/sdscli"
 __description__ = "Command line interface for SDSKit"
